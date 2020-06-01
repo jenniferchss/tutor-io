e34980876e87be31ca901e88614329dff76ea50b
jennifer <jenniferchessa@gmail.com>
2020-06-01 20:38:35 +0800

    edit jennifer navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
 return (<div>
-    <img className="background-img" src="./images/background-img.jpeg" width="100%" alt="background-img" loading="lazy"></img>
+    <img className="background-img" src={require("./images/background-img.jpeg")} width="100%" alt="background-img" loading="lazy"></img>
     
     </div>);
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,43 +1,59 @@
 import React from "react";
+//import logo from './images/logo.png';
 
 function Navbar() {
 
 return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark">
+        <a className="navbar-brand" href="#">
+            <img src={require('./images/logo.png')} width="90" alt="tutorio-logo" loading="lazy"></img>
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            
+                <ul className="navbar-nav navbar-center">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">About Us<span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">How It Works</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Modules
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="#">Faculty of Engineering</a>
+                            <a className="dropdown-item" href="#">Faculty of Science</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="#">School of Computing</a>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Testimony</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Join Us</a>
+                    </li>
+                </ul>
+            
+
+            <form className="search-box form-inline my-2 my-lg-0">
+                <input className="form-control form-control-sm mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-light btn-sm my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            
+                
+            <ul className="navbar-nav navbar-right justify-content-end">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Sign In</a>
+                </li>
+            </ul>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+    </nav>
 );
 }
 

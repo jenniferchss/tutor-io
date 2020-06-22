0f02e8d4e9ec4a7169671cc9894b9d9781ce43a5
jennifer <jenniferchessa@gmail.com>
2020-06-22 23:03:39 +0800

    edit jennifer signup page axios

--- tutorIO_back/server.js
+++ tutorIO_back/server.js
@@ -21,8 +21,8 @@ app.use(express.json());
  */
 app.use("/user", user);
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
 });
 
 

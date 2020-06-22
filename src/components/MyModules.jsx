c3b9d67405f7c8bd2f7463ecde5d0559432b10dc
jennifer <jenniferchessa@gmail.com>
2020-06-22 13:04:25 +0800

    edit jennifer dashboard etc pages

--- src/components/MyModules.jsx
+++ src/components/MyModules.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import SubNavbar from "./SubNavbar.jsx";
+//import SubNavbar from "./SubNavbar.jsx";
+import SideNav from "./SideNav";
 import TutorModules from './TutorModules.jsx';
 import TuteeModules from './TuteeModules.jsx';
 
 function MyModules() {
     return (<div className="dashboard">
-        <SubNavbar/>
         <div className="row">
-            <div className="col-lg-6">
-                <TutorModules/>
-            </div>
+            <SideNav />
+            
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <TutorModules/>
+                    </div>
 
-            <div className="col-lg-6">
-                <TuteeModules/>
-            </div>
+                    <div className="col-lg-6">
+                        <TuteeModules/>
+                    </div>
 
+                </div>
+            </main>
         </div>
     </div>);
 }

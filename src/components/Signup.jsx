6fd1b376f7afbe670660bf2490063589afe9cbf2
jennifer <jenniferchessa@gmail.com>
2020-06-23 19:46:24 +0800

    edit jennifer signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -33,59 +33,61 @@ function Signup() {
         event.preventDefault();
     }
     
-    return (<div className="text-center joinus-page" data-gr-c-s-loaded="true">
+    return (
+        <div className="text-center joinus-page" data-gr-c-s-loaded="true">
 
-    <form onSubmit={handleSubmit} className="login-form">
-        
-        <h1 className="joinus-title h3 mb-3 font-weight-normal">Join our Big Family!</h1>
-        
-        <label for="inputUsername" class="sr-only username-input">Username</label>
-        <input 
-            onChange={handleChangeUser}
-            type="username" 
-            id="inputUsername" 
-            class="form-control" 
-            placeholder="@username" 
-            value={username}
-            required autofocus
-        />
+            <form onSubmit={handleSubmit} className="login-form">
+                
+                <h1 className="joinus-title h3 mb-3 font-weight-normal">Join our Big Family!</h1>
+                
+                <label for="inputUsername" class="sr-only username-input">Username</label>
+                <input 
+                    onChange={handleChangeUser}
+                    type="username" 
+                    id="inputUsername" 
+                    class="form-control" 
+                    placeholder="@username" 
+                    value={username}
+                    required autofocus
+                />
 
-        <label for="inputEmail" class="sr-only email-input">Email address</label>
-        <input 
-            onChange={handleChangeEmail}
-            type="email" 
-            id="inputEmail" 
-            class="form-control" 
-            placeholder="Email address" 
-            value={emailaddress}
-            required 
-        />
-        
-        <label for="inputPassword" class="sr-only password-input">Password</label>
-        <input 
-            onChange={handleChangePassword}
-            type="password" 
-            id="inputPassword" 
-            class="form-control" 
-            placeholder="Password" 
-            value={password}
-            required 
-        />
-        
-        <label for="inputPassword" class="sr-only password-input">Confirm Password</label>
-        <input 
-            type="password" 
-            id="inputConfirmPassword" 
-            class="form-control" 
-            placeholder="Confirm password" 
-            required 
-        />
-        
-        <button className="btn btn-lg btn-info btn-block" type="submit">Sign up</button>
-        <p class="mt-5 mb-3 text-muted">Copyright © 2020</p>
+                <label for="inputEmail" class="sr-only email-input">Email address</label>
+                <input 
+                    onChange={handleChangeEmail}
+                    type="email" 
+                    id="inputEmail" 
+                    class="form-control" 
+                    placeholder="Email address" 
+                    value={emailaddress}
+                    required 
+                />
+                
+                <label for="inputPassword" class="sr-only password-input">Password</label>
+                <input 
+                    onChange={handleChangePassword}
+                    type="password" 
+                    id="inputPassword" 
+                    class="form-control" 
+                    placeholder="Password" 
+                    value={password}
+                    required 
+                />
+                
+                <label for="inputPassword" class="sr-only password-input">Confirm Password</label>
+                <input 
+                    type="password" 
+                    id="inputConfirmPassword" 
+                    class="form-control" 
+                    placeholder="Confirm password" 
+                    required 
+                />
+                
+                <button className="btn btn-lg btn-info btn-block" type="submit">Sign up</button>
+                <p class="mt-5 mb-3 text-muted">Copyright © 2020</p>
 
-    </form>
-</div>);
+            </form>
+        </div>
+    );
 }
-}
 
+export default Signup;
